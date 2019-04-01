adca483bb28dd458b35515feca12a88ce26b333b
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-04-01 17:40:54 -0400

    add prettier / eslint

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Home = () => (
   <div>
     <p>Hello Next.js - Home.</p>
     <Link href="/about">
-    <a>About Page</a>
+      <a>About Page</a>
     </Link>
   </div>
 );
