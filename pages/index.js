1a2747ff67ef6784fe66aed4a672fe38e0dd4871
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-04-02 11:17:22 -0400

    add isomorphic-unfetch, api tvmaze batman call

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,33 @@
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 import Layout from "../components/MyLayout";
 
-const PostLink = ({ title, id }) => (
-  <li>
-    <Link href={`/post?title=${title}`} as={`/p/${id}`}>
-      <a>{title}</a>
-    </Link>
-  </li>
-);
-
-const Home = () => (
+const Home = ({ shows }) => (
   <div>
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink id="hello-nextjs" title="Hello Next.js" />
-        <PostLink id="learn-nextjs" title="Learn Next.js" />
-        <PostLink id="deploy-nextjs" title="Deploy Next.js" />
+        {shows.map(({ id, name }) => (
+          <li key={id}>
+            <Link href={`/post?id=${id}`} as={`/p/${id}`}>
+              <a>{name}</a>
+            </Link>
+          </li>
+        ))}
       </ul>
     </Layout>
   </div>
 );
+
+Home.getInitialProps = async function getShowData() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log("Show data fetched. Count:", data.length);
+
+  return {
+    shows: data.map(entry => entry.show),
+  };
+};
 
 export default Home;
