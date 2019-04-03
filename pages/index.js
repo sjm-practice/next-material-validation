4531adc5f0cc17ee4c8255cc7371e9ed620a4732
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-04-02 20:09:02 -0400

    use some styled-jsx for practice

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,52 @@ import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 import Layout from "../components/MyLayout";
 
+const ShowLink = ({ show: { id, name } }) => (
+  <li>
+    <Link href={`/post?id=${id}`} as={`/p/${id}`}>
+      <a>{name}</a>
+    </Link>
+    <style jsx>
+      {`
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+          font-family: "Arial";
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}
+    </style>
+  </li>
+);
+
 const Home = ({ shows }) => (
   <div>
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        {shows.map(({ id, name }) => (
-          <li key={id}>
-            <Link href={`/post?id=${id}`} as={`/p/${id}`}>
-              <a>{name}</a>
-            </Link>
-          </li>
+        {shows.map(show => (
+          <ShowLink key={show.id} show={show} />
         ))}
       </ul>
+      <style jsx>
+        {`
+          h1 {
+            font-family: "Arial";
+          }
+
+          ul {
+            padding: 0;
+          }
+        `}
+      </style>
     </Layout>
   </div>
 );
