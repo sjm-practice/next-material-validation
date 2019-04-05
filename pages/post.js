966381b2a31e3cbe511160bfcc1b8f3fa32c5e49
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-04-05 17:38:45 -0400

    add card style to post page

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,40 @@
 import React from "react";
 import fetch from "isomorphic-unfetch";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
-const Post = ({ show }) => (
+const styles = () => ({
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+});
+
+const Post = ({ show, classes }) => (
   <>
-    <h1>{show.name}</h1>
-    <p>{show.summary.replace(/<[/]?p>/g, "")}</p>
-    <img src={show.image.medium} alt={show.name} />
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.cardMedia}
+        image={show.image.medium}
+        title={show.name}
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography gutterBottom variant="h5" component="h2">
+          {show.name}
+        </Typography>
+        <Typography>{show.summary.replace(/<[/]?p>/g, "")}</Typography>
+      </CardContent>
+    </Card>
   </>
 );
 
@@ -20,4 +49,4 @@ Post.getInitialProps = async function getShowData(context) {
   return { show };
 };
 
-export default Post;
+export default withStyles(styles)(Post);
