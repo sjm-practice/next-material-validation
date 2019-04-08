353a0850008ec08b1c43c8d5e47366ab7a520ebf
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-04-08 11:24:56 -0400

    remove non default palette colors

--- src/getPageContext.js
+++ src/getPageContext.js
@@ -1,23 +1,9 @@
 import { SheetsRegistry } from "jss";
 import { createMuiTheme, createGenerateClassName } from "@material-ui/core/styles";
-import purple from "@material-ui/core/colors/purple";
-import green from "@material-ui/core/colors/green";
 
 // A theme with custom primary and secondary color.
 // It's optional.
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700],
-    },
-    secondary: {
-      light: green[300],
-      main: green[500],
-      dark: green[700],
-    },
-  },
   typography: {
     useNextVariants: true,
   },
